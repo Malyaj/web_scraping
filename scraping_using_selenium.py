547090ecf3c2a9d200b53d63b35c1f91aba075a6
Malyaj Srivastav <Malyaj@users.noreply.github.com>
2019-01-18 19:44:46 +0530

    Update scraping_using_selenium.py

--- scraping_using_selenium.py
+++ scraping_using_selenium.py
@@ -1,9 +1,10 @@
-SSO = "SSO here"
-key = "key here"
+# credentials to be entered from a GUI
+SSO = ""
+key = ""
 
 import os
 from selenium import webdriver
-
+import urllib
 
 chromedriver_path = r"D:\Users\703143501\AppData\Local\Programs\Python\Python37-32\chromedriver.exe"
 # https://sites.google.com/a/chromium.org/chromedriver/
@@ -11,7 +12,7 @@ chromedriver_path = r"D:\Users\703143501\AppData\Local\Programs\Python\Python37-
 # "D:\Users\703143501\AppData\Local\Programs\Python\Python37-32\chromedriver.exe"
 
 wf = 170164299
-url = f"http://supportcentral.ge.com/caseforms/sup_myforms.asp?form_doc_id={wf}"
+url = f"https://supportcentral.ge.com/caseforms/sup_myforms.asp?form_doc_id={wf}"
 
 download_path = r"D:\Users\703143501\Downloads"
 options = webdriver.ChromeOptions() 
@@ -29,24 +30,26 @@ submit_field = driver.find_element_by_name("submitFrm")
 submit_field.click()
 
 # listing the frames on the page
-frame_list = driver.find_elements_by_tag_name('frame')
+#frame_list = driver.find_elements_by_tag_name('frame')
+#for each in frame_list:
+#    print(each)
 
-for each in frame_list:
-    print(each)
 
-'''
-# sqitching to frame
+# switching to frame
 frame_id = driver.find_element_by_id('mycase')
 driver.switch_to.frame(frame_id)
 
-xpath = """//*[@id="divCond_13756759"]/td[2]/a"""
-elem = driver.find_element_by_xpath(xpath)
-#print(elem.text)
-elem.click()
+#xpath = """//*[@id="divCond_13756759"]/td[2]/a"""
+#elem = driver.find_element_by_xpath(xpath).click()
+
+#link = "https://libraries.ge.com/systemfiledownload?fileid=942004535101" 
+#urllib.urlretrieve(link)
+href="https://libraries.ge.com/systemfiledownload?fileid=942004535101"
+driver.get(href)
+# href="http://libraries.ge.com/systemfiledownload?fileid=942004535101"
+#t = elem.text
 
 
-
+print("Done till end!")
 # closing the browser
-driver.close()
-
-'''
+#driver.close()
