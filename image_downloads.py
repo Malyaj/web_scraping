c171623c97bc414da76ded22f41ac83d05130a5d
Malyaj Srivastav <Malyaj@users.noreply.github.com>
2019-04-26 23:38:33 +0530

    Create image_downloads.py

---
+++ image_downloads.py
@@ -0,0 +1,6 @@
+from urllib.request import urlopen, Request
+
+req = urllib.request.Request(img_url, headers=headers)
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+
